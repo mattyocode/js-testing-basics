cbd607b1dbb0e9694154cdfc532507f1005b0d76
mattyocode <matthewoliver@live.co.uk>
2021-03-26 15:11:15 +0000

    test: truthy and falsy tests for isAnagram

--- anagram.test.js
+++ anagram.test.js
@@ -1,5 +1,13 @@
 const isAnagram = require('./anagram');
 
 test('isAnagram function exists', () => {
-    expect(isAnagram).toBeDefined();
+    expect(typeof isAnagram).toEqual('function');
 });
+
+test('"cinema" is an anagram of "iceman"', () => {
+    expect(isAnagram('cinema', 'iceman')).toBeTruthy();
+});
+
+test('"dog" is an anagram of "iceman"', () => {
+    expect(isAnagram('dog', 'iceman')).toBeFalsy();
+})
